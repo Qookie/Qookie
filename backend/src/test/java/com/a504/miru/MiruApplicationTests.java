b721bba272df998ab28da4d7d38d70db20ddbb18
김동훈 <hunn000@gmail.com>
2023-10-17 09:58:18 +0900

    init

---
+++ backend/src/test/java/com/a504/miru/MiruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a504.miru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
