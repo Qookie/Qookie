2197a9a26e16bbf7f4f2a171d4df5555dcb59ca9
윤유탁 <dbsdbxkrzz@naver.com>
2023-10-19 15:00:47 +0900

    ################ # (<지라 이슈번호>) <타입>: <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat: 로그인 기능 추가 (지라 이슈번호) feat : domain setting # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    # Optional
    도메인별 엔티티 생성 및 프로젝트 구조 설정
    ####Type List####
    # feat    : 새로운 기능 추가
    # fix     : 버그 수정
    # docs    : 문서 수정
    # test    : 테스트 코드 추가
    # refact  : 코드 리팩토링
    # style   : 코드 의미에 영향을 주지 않는 변경사항
    # chore   : 빌드 부분 혹은 패키지 매니저 수정사항
    # design  : CSS 등 사용자 UI 디자인 변경
    # comment : 필요한 주석 추가 및 변경
    # rename  : 파일 또는 폴더 명을 수정하거나 옮기는 작업만인 경우
    # remove  : 파일을 삭제하는 작업만 수행한 경우
    # ci      : CI 관련 설정에 대한 변경
    # build   : Build 관련 설정에 대한 변경
    ################

---
+++ backend/src/main/java/com/a504/miru/domain/quest/entity/Quest.java
@@ -0,0 +1,34 @@
+package com.a504.miru.domain.quest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Quest {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "quest_id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "content")
+	private String content;
+
+	@Column(name = "media")
+	private String media;
+}
