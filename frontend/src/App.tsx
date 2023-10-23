a8b9c20c272f1f0426c473f98f202e6620837e84
이국신 <nw4611@naver.com>
2023-10-23 15:30:57 +0900

    chore: 모바일 레이아웃 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import Router from './router/Router';
 import GlobalStyle from './styles/GlobalStyle';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
+    <Layout>
       <GlobalStyle />
       <Router />
-    </div>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+  max-width: 430px;
+  width: 100%;
+  margin: 0 auto;
+  min-height: 100vh;
+`;
 
 export default App;
