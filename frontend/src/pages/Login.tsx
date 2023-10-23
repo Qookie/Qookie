0efa9ed4a14d175be644ce0b17da03030e1d7bca
이국신 <nw4611@naver.com>
2023-10-23 15:36:44 +0900

    chore: 린팅 적용

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,36 +1,40 @@
-import { signInWithRedirect, GoogleAuthProvider, OAuthProvider, getRedirectResult } from "firebase/auth"
-import { auth } from "../firebase/firebaseConfig"
-import { useEffect } from "react"
+import {
+  signInWithRedirect,
+  GoogleAuthProvider,
+  OAuthProvider,
+  getRedirectResult,
+} from 'firebase/auth';
+import { auth } from '../firebase/firebaseConfig';
+import { useEffect } from 'react';
 
 const Login = () => {
-
   const googleLogin = () => {
-		signInWithRedirect(auth, new GoogleAuthProvider())
-    console.log("googlelogin")
-  }
+    signInWithRedirect(auth, new GoogleAuthProvider());
+    console.log('googlelogin');
+  };
   const kakaoLogin = () => {
-		const provider = new OAuthProvider("oidc.kakao")
-		signInWithRedirect(auth, provider)
-    console.log("kakaologin")
-  }
+    const provider = new OAuthProvider('oidc.kakao');
+    signInWithRedirect(auth, provider);
+    console.log('kakaologin');
+  };
 
-	useEffect(() => {
-		getRedirectResult(auth)
-			.then((res) => {
-				console.log("RES", res)
-				// send accessToken, refreshToken, uid to backend
-			})
-			.catch((err)=>{
-				console.log("ERR", err)
-			})
-	})
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then((res) => {
+        console.log('RES', res);
+        // send accessToken, refreshToken, uid to backend
+      })
+      .catch((err) => {
+        console.log('ERR', err);
+      });
+  });
 
   return (
     <>
       <img src="/google.png" alt="googleLogin" onClick={googleLogin} />
       <img src="/kakao.png" alt="kakaoLogin" onClick={kakaoLogin} />
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
