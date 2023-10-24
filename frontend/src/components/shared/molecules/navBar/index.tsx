7271fc23eaaef18dc10b6f6c6a2bca98b9095f8f
조원주 <dnjswn9178@hanmail.net>
2023-10-24 15:06:36 +0900

    rename: delete styled code and move to index

--- frontend/src/components/shared/molecules/navBar/index.tsx
+++ frontend/src/components/shared/molecules/navBar/index.tsx
@@ -1,25 +1,66 @@
-import * as S from './index.styled';
+import styled from 'styled-components';
 import { HomeIcon, CalendarIcon, EnvelopeIcon, UserIcon } from '@heroicons/react/20/solid';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <S.NavContainer>
-      <S.NavItem to={'/home'}>
+    <NavContainer>
+      <NavItem to={'/home'}>
         <HomeIcon />
-        <S.NavName>홈</S.NavName>
-      </S.NavItem>
-      <S.NavItem to={'/calendar'}>
+        <NavName>홈</NavName>
+      </NavItem>
+      <NavItem to={'/calendar'}>
         <CalendarIcon />
-        <S.NavName>캘린더</S.NavName>
-      </S.NavItem>
-      <S.NavItem to={'/mind'}>
+        <NavName>캘린더</NavName>
+      </NavItem>
+      <NavItem to={'/mind'}>
         <EnvelopeIcon />
-        <S.NavName>마음함</S.NavName>
-      </S.NavItem>
-      <S.NavItem to={'/mypage'}>
+        <NavName>마음함</NavName>
+      </NavItem>
+      <NavItem to={'/mypage'}>
         <UserIcon />
-        <S.NavName>마이페이지</S.NavName>
-      </S.NavItem>
-    </S.NavContainer>
+        <NavName>마이페이지</NavName>
+      </NavItem>
+    </NavContainer>
   );
 }
+
+export const NavContainer = styled.div`
+  width: 100%;
+  height: 3.4rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: var(--MR_WHITE);
+  box-shadow: 0px -4px 8px 0px rgba(224, 224, 224, 0.15);
+  padding: 1.1rem 0;
+  z-index: 5;
+`;
+
+export const NavItem = styled(NavLink)`
+  width: 25%;
+  height: 100%;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: var(--MR_GRAY1);
+  &.active {
+    color: var(--MR_BLACK);
+    position: relative;
+  }
+  & > svg {
+    width: 1.6rem;
+    height: 1.6rem;
+  }
+`;
+
+export const NavName = styled.div`
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 600;
+  margin-top: 0.4rem;
+`;
