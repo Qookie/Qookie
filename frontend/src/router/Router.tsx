e94b6909c4ba36d775420df2360dfefbd3b3dfae
조원주 <dnjswn9178@hanmail.net>
2023-10-24 17:49:04 +0900

    feat: add Header Wrapper component

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Login from '../pages/Login';
 import NavBar from '../components/shared/molecules/navBar';
 import Home from '../pages/Home';
+import Header from '../components/shared/molecules/header';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <HeaderWrapper />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
@@ -16,6 +18,27 @@ const Router = () => {
       <NavBarWrapper />
     </BrowserRouter>
   );
+};
+
+const HeaderWrapper = () => {
+  const location = useLocation();
+
+  const checkPath = () => {
+    switch (location.pathname) {
+      case '/home':
+        return 'home';
+      case '/mind':
+        return '마음함';
+      case '/calendar':
+        return '캘린더';
+      case '/mypage':
+        return '마이페이지';
+      default:
+        return location.pathname;
+    }
+  };
+
+  return <Header page={checkPath()} />;
 };
 
 const NavBarWrapper = () => {
