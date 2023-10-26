84fa07a0c6a18534d8848982d851791266743367
윤유탁 <dbsdbxkrzz@naver.com>
2023-10-26 15:16:26 +0900

    refactor : 퀘스트 완료 기능 리팩토링

--- backend/src/main/java/com/a504/qookie/domain/quest/repository/QuestRepository.java
+++ backend/src/main/java/com/a504/qookie/domain/quest/repository/QuestRepository.java
@@ -1,9 +1,11 @@
 package com.a504.qookie.domain.quest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.a504.qookie.domain.quest.entity.Quest;
 
 public interface QuestRepository extends JpaRepository<Quest, Long> {
-
+	Optional<Quest> findByName(String questName);
 }
