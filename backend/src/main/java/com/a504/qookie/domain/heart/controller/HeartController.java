cccbdca0a9cd790547ef2b4da7346133f0abcfe6
kys981207@naver.com <kys981207@naver.com>
2023-10-26 15:45:09 +0900

    feat: 지난 마음 API 작성

--- backend/src/main/java/com/a504/qookie/domain/heart/controller/HeartController.java
+++ backend/src/main/java/com/a504/qookie/domain/heart/controller/HeartController.java
@@ -1,13 +1,16 @@
 package com.a504.qookie.domain.heart.controller;
 
 import com.a504.qookie.domain.heart.dto.HeartRequest;
+import com.a504.qookie.domain.heart.dto.HeartResponse;
 import com.a504.qookie.domain.heart.service.HeartService;
 import com.a504.qookie.global.response.BaseResponse;
 import com.a504.qookie.global.security.CustomMemberDetails;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +31,14 @@ public class HeartController {
         heartService.create(customMemberDetails.getMember(), heartRequest);
 
         return BaseResponse.ok(HttpStatus.OK, "heart create OK");
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> list(
+            @AuthenticationPrincipal CustomMemberDetails customMemberDetails) {
+
+        List<HeartResponse> heartResponses = heartService.list(customMemberDetails.getMember());
+
+        return BaseResponse.okWithData(HttpStatus.OK, "heart list OK", heartResponses);
     }
 }
--- backend/src/main/java/com/a504/qookie/domain/heart/service/HeartService.java
+++ backend/src/main/java/com/a504/qookie/domain/heart/service/HeartService.java
@@ -1,10 +1,13 @@
 package com.a504.qookie.domain.heart.service;
 
 import com.a504.qookie.domain.heart.dto.HeartRequest;
+import com.a504.qookie.domain.heart.dto.HeartResponse;
 import com.a504.qookie.domain.heart.entity.Heart;
 import com.a504.qookie.domain.heart.repository.HeartRepository;
 import com.a504.qookie.domain.member.entity.Member;
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +23,18 @@ public class HeartService {
         Heart heart = new Heart(member, heartRequest);
 
         heartRepository.save(heart);
+    }
+
+    @Transactional
+    public List<HeartResponse> list(Member member) {
+
+        List<Heart> hearts = heartRepository.findByMember(member);
+
+        List<HeartResponse> heartResponses = new ArrayList<>();
+        for (Heart heart:hearts) {
+            heartResponses.add(new HeartResponse(heart));
+        }
+
+        return heartResponses;
     }
 }
