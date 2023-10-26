24fef492ed046589dd1816549cdcb5f944d0d273
kys981207@naver.com <kys981207@naver.com>
2023-10-26 15:22:05 +0900

    fix: 마음 보내기 API 수정

--- backend/src/main/java/com/a504/qookie/domain/heart/dto/HeartRequest.java
+++ backend/src/main/java/com/a504/qookie/domain/heart/dto/HeartRequest.java
@@ -1,9 +1,5 @@
 package com.a504.qookie.domain.heart.dto;
 
-import com.a504.qookie.domain.cookie.entity.Eye;
-import com.a504.qookie.domain.cookie.entity.Mouth;
-import java.util.List;
-
 public record HeartRequest(
     int category,
     String content
