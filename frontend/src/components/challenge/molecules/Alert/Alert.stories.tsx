5cf392203bd8da458d0c2a9de9b6ae1d3a3a2a5e
이현복 <nexusin@naver.com>
2023-10-26 17:34:38 +0900

    feat: Toast 알림 스토리 구현

---
+++ frontend/src/components/challenge/molecules/Alert/Alert.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Toast from '.';
+
+const meta: Meta<typeof Toast> = {
+  component: Toast,
+  title: 'Toast',
+};
+
+export default meta;
+type Story = StoryObj<typeof Toast>;
+
+export const Default: Story = {
+  args: {
+  },
+
+  render: (args) => <Toast {...args} />,
+};
