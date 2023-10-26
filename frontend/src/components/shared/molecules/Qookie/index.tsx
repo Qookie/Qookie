16f6b3c27b95ecd2d49e6b21a103994e5306a8ea
조원주 <dnjswn9178@hanmail.net>
2023-10-26 14:39:16 +0900

    feat: implement Qookie Component

---
+++ frontend/src/components/shared/molecules/Qookie/index.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Dough2 } from '../../../../assets/svgs';
+import Background from '../../atoms/Background';
+import Eye from '../../atoms/Eye';
+import Mouth from '../../atoms/Mouth';
+
+export default function Qookie() {
+  return (
+    <QookieContainer>
+      <Background />
+      <DoughContainer>
+        <Dough2 />
+      </DoughContainer>
+      <Eye />
+      <Mouth />
+    </QookieContainer>
+  );
+}
+
+const QookieContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const DoughContainer = styled.div`
+  z-index: 1;
+`;
