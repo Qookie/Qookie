b3b59e3cb41af0d764372375541396cf6a52b801
조원주 <dnjswn9178@hanmail.net>
2023-10-26 14:48:56 +0900

    feat: add Qookie and status card

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import Qookie from '../components/shared/molecules/Qookie';
+import styled from 'styled-components';
+import StatusCard from '../components/shared/organisms/StatusCard';
+import { useRecoilState } from 'recoil';
+import { QookieInfoState } from '../modules/qookie';
+
 const Home = () => {
-  return <>HOME</>;
+  const [qookie, setqookie] = useRecoilState(QookieInfoState);
+
+  return (
+    <HomeContainer>
+      <Qookie />
+      <ContentsWrapper>
+        <StatusCard level={qookie.level} exp={qookie.exp} />
+      </ContentsWrapper>
+    </HomeContainer>
+  );
 };
 
 export default Home;
+
+const HomeContainer = styled.div`
+  padding-top: 4rem;
+`;
+
+const ContentsWrapper = styled.div`
+  padding: 3rem 1rem 0 1rem;
+`;
