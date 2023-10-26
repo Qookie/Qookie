932d4551ceaa4ae18083888302373e344808df06
조원주 <dnjswn9178@hanmail.net>
2023-10-26 13:24:49 +0900

    feat: add Qookie svg files

--- frontend/src/assets/svgs/index.ts
+++ frontend/src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
+// level icon
 export { ReactComponent as Lv1 } from './lv1.svg';
 export { ReactComponent as Lv2 } from './lv2.svg';
 export { ReactComponent as Lv3 } from './lv3.svg';
@@ -5,4 +6,15 @@ export { ReactComponent as Lv4 } from './lv4.svg';
 export { ReactComponent as Lv5 } from './lv5.svg';
 export { ReactComponent as Lv6 } from './lv6.svg';
 
+// icon
 export { ReactComponent as CoinLogo } from './coin.svg';
+
+// background
+export { ReactComponent as Bg1 } from './bg1.svg';
+
+// dough
+export { ReactComponent as Dough2 } from './dough2.svg';
+
+// item
+export { ReactComponent as Eye1 } from './eye1.svg';
+export { ReactComponent as Mouth1 } from './mouth1.svg';
