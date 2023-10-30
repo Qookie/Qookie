9a28c909da5db866dafe11edef6ef0ffed907990
kys981207@naver.com <kys981207@naver.com>
2023-10-30 17:11:17 +0900

    fix: 아이템 목록 조회 API 아이템의 id를 Response에 추가

--- backend/src/main/java/com/a504/qookie/domain/item/dto/ItemResponse.java
+++ backend/src/main/java/com/a504/qookie/domain/item/dto/ItemResponse.java
@@ -3,12 +3,13 @@ package com.a504.qookie.domain.item.dto;
 import com.a504.qookie.domain.item.entity.Item;
 
 public record ItemResponse(
+        Long id,
         String name,
         String media,
         int price,
         Boolean isNew
 ) {
     public ItemResponse(Item item) {
-        this(item.getName(), item.getMedia(), item.getPrice(), item.getIsNew());
+        this(item.getId(), item.getName(), item.getMedia(), item.getPrice(), item.getIsNew());
     }
 }
