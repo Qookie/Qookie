2d26671aebd553652a403d90d52b973dc5118338
이현복 <nexusin@naver.com>
2023-10-30 15:33:11 +0900

    chore: Toast 알림 로직 수정

--- frontend/src/components/challenge/molecules/Alert/index.tsx
+++ frontend/src/components/challenge/molecules/Alert/index.tsx
@@ -1,35 +1,15 @@
 import styled from 'styled-components';
 import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ReactComponent as Qookie } from '../../../../assets/qookie.svg';
+import { ReactComponent as Qookie } from '../../../../assets/svgs/qookie.svg';
 
 interface ToastProps {
-  type: 'default' | 'levelup' | 'achieve' | 'earn'
+  type?: 'default' | 'levelup' | 'achieve' | 'earn'
   title?: string
   content?: string
 }
 
-export default function Toast({ type='default', title, content }: ToastProps) {
-  switch (type) {
-    case 'levelup':
-      title = '레벨업!';
-      content = '더 열심히 해서 만렙을 향해 달려가보세요!';
-      break;
-    case 'achieve':
-      title = '챌린지 달성!';
-      content = '시간 맞춰 기상 배지를 받아보세요.';
-      break;
-    case 'earn':
-      title = '10 포인트 획득!🌟';
-      content = '축하합니다! 포인트를 획득하셨습니다.';
-      break;
-    case 'default':
-      title = 'Qookie에 오신 것을 환영합니다!';
-      content = 'Qookie와 함께 건강해지시길 바랍니다!';
-      break;
-
-  }
-  
+export default function ShowToast({ type='default', title, content }: ToastProps) {
   function Message() {
     return (
     <>
@@ -44,16 +24,36 @@ export default function Toast({ type='default', title, content }: ToastProps) {
     closeButton: false,
     icon: <Qookie />,
     style: {
-      width: '100%',
       height: '72px',
       background: 'rgba(1, 17, 38, 0.75)',
       borderRadius: '8px',
     },
   });
+  
+  switch (type) {
+    case 'levelup':
+      title = '레벨업!';
+      content = '더 열심히 해서 만렙을 향해 달려가보세요!';
+      notify();
+      break;
+    case 'achieve':
+      title = '챌린지 달성!';
+      content = '시간 맞춰 기상 배지를 받아보세요.';
+      notify();
+      break;
+    case 'earn':
+      title = '10 포인트 획득!🌟';
+      content = '축하합니다! 포인트를 획득하셨습니다.';
+      notify();
+      break;
+    case 'default':
+      title = 'Qookie에 오신 것을 환영합니다';
+      content = 'Qookie와 함께 건강해지시길 바랍니다!';
+      notify();
+      break;
+  }
 
   return (
-    <div>
-      <button onClick={notify}>테스트!</button>
       <StyledToast
         position="top-center" // 알람 위치 지정
         autoClose={3000} // 자동 off 시간
@@ -61,11 +61,10 @@ export default function Toast({ type='default', title, content }: ToastProps) {
         transition={Zoom} // 알림 등장 애니메이션
         draggablePercent={70} // 드래그로 닫기
       />
-    </div>
   );
 }
 const MessageTitle = styled.div`
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 600;
   color: var(--MR_WHITE);
   margin: 5px 0px;
@@ -83,6 +82,7 @@ const StyledToast = styled(ToastContainer)`
     display: flex;
     justify-content: left;
     align-items: center;
+    margin: 1rem 1rem 0rem 1rem;
   }
 
   .Toastify__toast-icon {
