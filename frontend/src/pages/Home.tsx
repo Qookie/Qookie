a07486e3953e311317a4ce52077ecef1fdd505fa
조원주 <dnjswn9178@hanmail.net>
2023-10-30 15:02:50 +0900

    feat: add quest buttons

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,18 +4,29 @@ import { QookieInfoState } from '../modules/qookie';
 import { useEffect } from 'react';
 import { qookieApi } from '../api';
 import QookieStatus from '../components/shared/organisms/QookieStatus';
+import QuestList from '../components/home/organisms/QuestList';
+import HomeButton from '../components/home/molecules/HomeButton';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const [qookie, setQookie] = useRecoilState(QookieInfoState);
 
-  // qookie Api 수정 후 setQookie수정 필요
-  // useEffect(() => {
-  //   qookieApi.getQookieInfo().then((res) => setQookie({...qookie, ...res}));
-  // });
+  // 로그인 후 setQookie 확인 필요
+  useEffect(() => {
+    qookieApi.getQookieInfo().then((res) => setQookie({ ...qookie, ...res }));
+  }, []);
 
   return (
     <HomeContainer>
       <QookieStatus {...qookie} />
+      <QuestContainer>
+        <ButtonContainer>
+          <HomeButton title="출석체크" />
+          <HomeButton title="챌린지" />
+        </ButtonContainer>
+        <QuestList title="일일 퀘스트" />
+        <QuestList title="추가 퀘스트" />
+      </QuestContainer>
     </HomeContainer>
   );
 };
@@ -23,3 +34,15 @@ const Home = () => {
 export default Home;
 
 const HomeContainer = styled.div``;
+
+const QuestContainer = styled.div`
+  display: grid;
+  padding: 2.5rem 1rem 6rem 1rem;
+  gap: 3rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
