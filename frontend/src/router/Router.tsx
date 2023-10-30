cdd83476786d435b7b403c20e5f6380fdaeb8364
이국신 <nw4611@naver.com>
2023-10-30 10:07:49 +0900

    feat: 쿠키 초기화 페이지 라우터 추가

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -3,6 +3,7 @@ import Login from '../pages/Login';
 import NavBar from '../components/shared/molecules/NavBar';
 import Home from '../pages/Home';
 import Header from '../components/shared/molecules/Header';
+import InitQookie from '../pages/InitQookie';
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <HeaderWrapper />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/init" element={<InitQookie />} />
         <Route path="/home" element={<Home />} />
         <Route path="/calendar" element={<Login />} />
         <Route path="/mind" element={<Login />} />
@@ -32,6 +34,8 @@ const HeaderWrapper = () => {
     case '/mypage':
       return <Header page="tab" title="마이페이지" />;
     case '/':
+      return <></>;
+    case '/init':
       return <></>;
     default:
       return <Header page="default" />;
