785d19df6c688e6bf65c337c932e2450adba48d9
이현복 <nexusin@naver.com>
2023-10-31 15:01:37 +0900

    chore: Toast 함수명 수정 및 불필요 코드 제거

--- frontend/src/components/challenge/molecules/Alert/index.tsx
+++ frontend/src/components/challenge/molecules/Alert/index.tsx
@@ -3,72 +3,36 @@ import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ReactComponent as Qookie } from '../../../../assets/svgs/qookie.svg';
 
-interface ToastProps {
-  type?: 'default' | 'levelup' | 'achieve' | 'earn'
-  title?: string
-  content?: string
-}
-
-export default function ShowToast({ type='default', title, content }: ToastProps) {
-  function Message() {
-    return (
+export default function Toast() {
+  toast.info(
     <>
-      <MessageTitle>{title}</MessageTitle>
-      <MessageContent>{content}</MessageContent>
-    </>
-    );
-  };
-
-  const notify = () => toast.info(<Message />, {
-    className: 'toast-message',
-    closeButton: false,
-    icon: <Qookie />,
-    style: {
-      height: '72px',
-      background: 'rgba(1, 17, 38, 0.75)',
-      borderRadius: '8px',
-    },
-  });
-  
-  switch (type) {
-    case 'levelup':
-      title = '레벨업!';
-      content = '더 열심히 해서 만렙을 향해 달려가보세요!';
-      notify();
-      break;
-    case 'achieve':
-      title = '챌린지 달성!';
-      content = '시간 맞춰 기상 배지를 받아보세요.';
-      notify();
-      break;
-    case 'earn':
-      title = '10 포인트 획득!🌟';
-      content = '축하합니다! 포인트를 획득하셨습니다.';
-      notify();
-      break;
-    case 'default':
-      title = 'Qookie에 오신 것을 환영합니다';
-      content = 'Qookie와 함께 건강해지시길 바랍니다!';
-      notify();
-      break;
-  }
+      <MessageTitle>제목</MessageTitle>
+      <MessageContent>내용</MessageContent>
+    </>,
+  );
 
   return (
+    <>
       <StyledToast
         position="top-center" // 알람 위치 지정
-        autoClose={3000} // 자동 off 시간
+        autoClose={3000} // 자동 off 시간 - 3초 뒤
         hideProgressBar={true} // 진행시간바 숨김
+        closeButton={false} // 닫기 버튼 숨김
         transition={Zoom} // 알림 등장 애니메이션
-        draggablePercent={70} // 드래그로 닫기
+        draggablePercent={70} // 좌우 드래그로 닫기
+        icon={<Qookie />} // 아이콘 SVG
+        newestOnTop={true} // 새로운 알림 위치 위로
       />
+    </>
   );
 }
+
 const MessageTitle = styled.div`
   font-size: 18px;
   font-weight: 600;
   color: var(--MR_WHITE);
   margin: 5px 0px;
-  `;
+`;
 
 const MessageContent = styled.div`
   font-size: 14px;
@@ -78,20 +42,26 @@ const MessageContent = styled.div`
 `;
 
 const StyledToast = styled(ToastContainer)`
+  /* Toast 컨테이너 */
   .Toastify__toast {
     display: flex;
     justify-content: left;
     align-items: center;
     margin: 1rem 1rem 0rem 1rem;
+    height: 72px;
+    background: rgba(1, 17, 38, 0.75);
+    border-radius: 8px;
   }
 
-  .Toastify__toast-icon {
-    width: 42px;
-    height: 42px;
-  }
-
+  /* Toast 컨테이너 내부 div */
   .Toastify__toast-body {
     margin: 0 0;
     flex: none;
+  }
+
+  /* Toast 컨테이너 내부 아이콘 */
+  .Toastify__toast-icon {
+    width: 42px;
+    height: 42px;
   }
 `;
