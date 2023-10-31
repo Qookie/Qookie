a7ce6371cdf08df71b27bae6594eaa1ee4ff3c66
윤유탁 <dbsdbxkrzz@naver.com>
2023-10-31 10:33:31 +0900

    feat : redis 통해 월간 챌린지, 뱃지 챌린지 값 업데이트 및 완료 검증 코드 추가

--- backend/src/main/java/com/a504/qookie/domain/quest/service/QuestService.java
+++ backend/src/main/java/com/a504/qookie/domain/quest/service/QuestService.java
@@ -1,5 +1,8 @@
 package com.a504.qookie.domain.quest.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.a504.qookie.domain.cookie.entity.Cookie;
@@ -22,6 +25,7 @@ public class QuestService {
 	private final QuestRepository questRepository;
 	private final MemberRepository memberRepository;
 	private final CookieRepository cookieRepository;
+	private final RedisTemplate<String, String> template;
 
 	public void completeQuest(Member member, String questName){
 		memberQuestRepository.save(
@@ -32,6 +36,7 @@ public class QuestService {
 				.build());
 		pointUpdate(member, 10);
 		updateExp(member);
+		checkChallenge(member, questName);
 	}
 
 	public void completeQuest(Member member, String questName, String imageName){
@@ -44,6 +49,7 @@ public class QuestService {
 				.build());
 		pointUpdate(member, 10);
 		updateExp(member);
+		checkChallenge(member, questName);
 	}
 
 	public void pointUpdate(Member member, int point){
@@ -118,5 +124,63 @@ public class QuestService {
 				cookie.updateExp(10);
 			}
 		}
+	}
+
+	public void checkChallenge(Member member, String questName){
+		if (questName.equals("WAKE") || questName.equals("EAT") || questName.equals("WALK")){ // 기상, 식사, 산책은 월간도 있어서
+			// 월간 챌린지 업데이트 및 알림 해주는 부분
+			 String cur_month = LocalDateTime.now().getMonth().toString();
+			 String monthly_challenge_key = member.getId() + ":" + cur_month + ":" +questName; // (유저PK):(이번달):(퀘스트이름)
+			 String monthly_challenge_value = template.opsForValue().get(monthly_challenge_key); // 클리어 횟수
+			 if (monthly_challenge_value == null){
+				template.opsForValue().set(monthly_challenge_key, "1");
+			 }else{
+				template.opsForValue().set(monthly_challenge_key, Long.parseLong(monthly_challenge_value) + 1L + "");
+			 }
+			 if (questName.equals("WAKE") || questName.equals("EAT")){
+				 if (Long.parseLong(template.opsForValue().get(monthly_challenge_key)) == 15){
+					 /* TODO : 알림 해주기 */
+				 }
+			 }else{
+				 if (Long.parseLong(template.opsForValue().get(monthly_challenge_key)) == 10){
+					 /* TODO : 알림 해주기 */
+				 }
+			 }
+		}
+
+		// 뱃지 챌린지 업데이트 및 알림해주는 기능
+		String badge_challenge_key = member.getId() + ":" + questName; // (유저PK):(퀘스트이름)
+		String badge_challenge_value = template.opsForValue().get(badge_challenge_key);
+		if (badge_challenge_value == null){
+			template.opsForValue().set(badge_challenge_key, "1");
+		}else{
+			template.opsForValue().set(badge_challenge_key, Long.parseLong(badge_challenge_value) + 1L + "");
+		}
+
+		if (questName.equals("PHOTO")){ // 하늘사진 찍기라면
+			// 위에서 업데이트 했기 때문에 null이 될 수 없음이 보장됨
+			Long cnt = Long.parseLong(template.opsForValue().get(badge_challenge_key));
+			if (cnt == 5){
+				/* TODO : 알림 해주기 */
+			}
+			if (cnt == 10){
+				/* TODO : 알림 해주기 */
+			}
+			if (cnt == 15){
+				/* TODO : 알림 해주기 */
+			}
+		}else{
+			Long cnt = Long.parseLong(template.opsForValue().get(badge_challenge_key));
+			if (cnt == 10){
+				/* TODO : 알림 해주기 */
+			}
+			if (cnt == 50){
+				/* TODO : 알림 해주기 */
+			}
+			if (cnt == 100){
+				/* TODO : 알림 해주기 */
+			}
+		}
+
 	}
 }
