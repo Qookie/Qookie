376eae2e0654438d51165f536a0deb74402bc952
조원주 <dnjswn9178@hanmail.net>
2023-10-31 13:51:09 +0900

    feat: implement dialog component

--- frontend/src/components/shared/molecules/Dialog/index.tsx
+++ frontend/src/components/shared/molecules/Dialog/index.tsx
@@ -1,27 +1,63 @@
-import { DialogHTMLAttributes } from 'react';
 import styled from 'styled-components';
 import Text from '../../atoms/Text';
 import Button from '../../atoms/Button';
 
-export interface DialogProps extends DialogHTMLAttributes<HTMLDialogElement> {
+export interface DialogProps {
   title: string;
   content: string;
   negative: string;
+  onNegativeClick: () => void;
   positive: string;
+  onPositiveClick: () => void;
+  isOpen: boolean;
+  onCloseRequest: () => void;
 }
 
-export default function Dialog({ title, content, negative, positive, ...props }: DialogProps) {
+export default function Dialog({
+  title,
+  content,
+  negative,
+  onNegativeClick,
+  positive,
+  onPositiveClick,
+  isOpen,
+  onCloseRequest,
+}: DialogProps) {
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <DialogContainer>
-      <Text typography="button">{title}</Text>
-      <Text typography="main">{content}</Text>
-      <ButtonContainer>
-        <Button size='medium' theme='transparent'>{positive}</Button>
-        <Button size='medium'>{negative}</Button>
-      </ButtonContainer>
-    </DialogContainer>
+    <>
+      <Backdrop onClick={onCloseRequest} />
+      <DialogContainer>
+        <Text typography="button">{title}</Text>
+        <Text typography="main">{content}</Text>
+        <ButtonContainer>
+          <Button size="medium" theme="transparent" onClick={onPositiveClick}>
+            {positive}
+          </Button>
+          <Button size="medium" onClick={onNegativeClick}>
+            {negative}
+          </Button>
+        </ButtonContainer>
+      </DialogContainer>
+    </>
   );
 }
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const DialogContainer = styled.div`
   border-radius: 1.25rem;
@@ -29,6 +65,8 @@ const DialogContainer = styled.div`
   background-color: var(--MR_WHITE);
   display: grid;
   gap: 0.7rem;
+  position: relative;
+  z-index: 11;
 `;
 
 const ButtonContainer = styled.div`
