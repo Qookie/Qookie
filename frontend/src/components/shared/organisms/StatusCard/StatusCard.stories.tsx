de406a32fd4d20e1938004c7249c43fa6396949e
조원주 <dnjswn9178@hanmail.net>
2023-10-31 13:56:34 +0900

    feat: add 50 level bake button

--- frontend/src/components/shared/organisms/StatusCard/StatusCard.stories.tsx
+++ frontend/src/components/shared/organisms/StatusCard/StatusCard.stories.tsx
@@ -1,8 +1,16 @@
 import { StoryObj, Meta } from '@storybook/react';
 import StatusCard from '.';
+import { BrowserRouter } from 'react-router-dom';
 
 const meta: Meta<typeof StatusCard> = {
   component: StatusCard,
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
   title: 'StatusCard',
 };
 
