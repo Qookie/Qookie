3c31ab2a91229a2af2758a239428f5acabb948e5
이국신 <nw4611@naver.com>
2023-11-02 13:35:49 +0900

    feat: 퀘스트 페이지 템플릿 구현

---
+++ frontend/src/components/quest/templates/QuestLayout/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import TitleLayout from '../../../shared/Template/TitleLayout';
+import Button from '../../../shared/atoms/Button';
+import styled from 'styled-components';
+
+type QuestStatus = 'DEFAULT' | 'COMPLETE' | 'SUCCESS';
+
+interface Props {
+  title: React.ReactNode;
+  desc: React.ReactNode;
+  Subcomponent: React.ReactNode;
+  completeButtonText: string;
+  completeQuest: () => void;
+  children?: React.ReactNode;
+  fetchQuestStatus: () => QuestStatus;
+}
+
+const ButtomThemeMap: {
+  [key in QuestStatus]: 'default' | 'finished' | 'disabled';
+} = {
+  DEFAULT: 'default',
+  SUCCESS: 'finished',
+  COMPLETE: 'disabled',
+};
+
+function QuestLayout({
+  title,
+  desc,
+  children,
+  completeButtonText,
+  Subcomponent,
+  fetchQuestStatus,
+  completeQuest,
+}: Props) {
+  const [questStatus, setQuestStatus] = useState<QuestStatus>('DEFAULT');
+
+  useEffect(() => {
+    const ret = fetchQuestStatus();
+    setQuestStatus(ret);
+  }, []);
+
+  const onClickComplete = async () => {
+    await completeQuest();
+    setQuestStatus('SUCCESS');
+  };
+
+  return (
+    <TitleLayout title={title} desc={desc}>
+      {children}
+      <ButtonConatainer>
+        {Subcomponent}
+        <Button theme={ButtomThemeMap[questStatus]} onClick={onClickComplete}>
+          {completeButtonText}
+        </Button>
+      </ButtonConatainer>
+    </TitleLayout>
+  );
+}
+
+const ButtonConatainer = styled.div`
+  padding: 1rem;
+  gap: 0.5rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export default QuestLayout;
