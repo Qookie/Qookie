1494c24de06a5e95a59300186e2ec2b8085112b3
kys981207@naver.com <kys981207@naver.com>
2023-11-02 10:23:47 +0900

    fix: Optional 처리

--- backend/src/main/java/com/a504/qookie/domain/cookie/dto/CookieResponse.java
+++ backend/src/main/java/com/a504/qookie/domain/cookie/dto/CookieResponse.java
@@ -2,7 +2,6 @@ package com.a504.qookie.domain.cookie.dto;
 
 import com.a504.qookie.domain.cookie.entity.Cookie;
 import java.time.LocalDateTime;
-
 public record CookieResponse(
         Long id,
         String name,
@@ -11,6 +10,7 @@ public record CookieResponse(
         int exp,
         int level,
         String body,
+        String extraBody,
         String eye,
         String mouth,
         String hat,
@@ -20,8 +20,9 @@ public record CookieResponse(
         String background
 ) {
 
-    public CookieResponse(Cookie cookie) {
-        this(cookie.getId(), cookie.getName(), cookie.getCreatedAt(), cookie.getStyle(), cookie.getExp(), cookie.getLevel(), cookie.getBody().getImage(),
+    public CookieResponse(Cookie cookie, String body, String extraBody) {
+        this(cookie.getId(), cookie.getName(), cookie.getCreatedAt(), cookie.getStyle(), cookie.getExp(), cookie.getLevel(), body,
+                extraBody,
                 cookie.getEye().getImage(), cookie.getMouth().getImage(), cookie.getHat().getMedia(), cookie.getTop().getMedia(),
                 cookie.getBottom().getMedia(), cookie. getShoe().getMedia(), cookie.getBackground().getMedia());
     }
