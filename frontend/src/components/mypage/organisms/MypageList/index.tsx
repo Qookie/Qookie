571a536eb0b71da406ba7b6e2ba2f766e60b0059
조원주 <dnjswn9178@hanmail.net>
2023-11-03 09:32:43 +0900

    feat: implement mypage

--- frontend/src/components/mypage/organisms/MypageList/index.tsx
+++ frontend/src/components/mypage/organisms/MypageList/index.tsx
@@ -1,29 +1,50 @@
 import styled from 'styled-components';
 import MypageListItem, { Mypage } from '../../molecules/MypageListItem';
+import Divider from '../../../shared/atoms/Divider';
 
 export default function MypageList() {
-  const mypageItemList: Mypage[] = [
+  const iconList: Mypage[] = [
     'deco',
     'badge',
     'myQookie',
     'qoin',
+  ];
+  const withoutIconList: Mypage[] = [
     'info',
     'notice',
     'privacy',
+  ];
+  const warnItemList: Mypage[] = [
     'logOut',
     'withDraw',
   ];
   return (
-    <ItemContainer>
-      {mypageItemList.map((item, index) => (
-        <>
+    <ListContainer>
+      {iconList.map((item, index) => (
+        <ItemContainer>
           <MypageListItem mypage={item} key={index} />
-        </>
+        </ItemContainer>
       ))}
-    </ItemContainer>
+      <Divider />
+      {withoutIconList.map((item, index) => (
+        <ItemContainer>
+          <MypageListItem mypage={item} key={index} />
+        </ItemContainer>
+      ))}
+      <Divider />
+      {warnItemList.map((item, index) => (
+        <ItemContainer>
+          <MypageListItem mypage={item} key={index} />
+        </ItemContainer>
+      ))}
+    </ListContainer>
   );
 }
 
-const ItemContainer = styled.div`
+const ListContainer = styled.div`
   width: 100%;
+`;
+
+const ItemContainer = styled.div`
+  padding: 1.35rem 1rem;
 `;
