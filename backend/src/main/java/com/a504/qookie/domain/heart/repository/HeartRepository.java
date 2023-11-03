9a02cdec6ec5067d13460d1bca1023bdccdaf49e
김동훈 <hunn000@gmail.com>
2023-11-03 13:00:15 +0900

    fix: add heart repository extends

--- backend/src/main/java/com/a504/qookie/domain/heart/repository/HeartRepository.java
+++ backend/src/main/java/com/a504/qookie/domain/heart/repository/HeartRepository.java
@@ -1,10 +1,11 @@
 package com.a504.qookie.domain.heart.repository;
 
 import com.a504.qookie.domain.heart.entity.Heart;
+import com.a504.qookie.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HeartRepository extends JpaRepository<Heart, Long> {
+import java.util.List;
+
+public interface HeartRepository extends JpaRepository<Heart, Long>, HeartRepositoryCustom {
     List<Heart> findAllByMember(Member member);
-
-
 }
