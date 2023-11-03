84fb4fa97a13689da04b45cdc845ba4ad8a43d94
김동훈 <hunn000@gmail.com>
2023-11-03 12:48:46 +0900

    feat: set docker-compose to build properly

--- backend/src/main/java/com/a504/qookie/global/rabbitMQ/RabbitMqConfig.java
+++ backend/src/main/java/com/a504/qookie/global/rabbitMQ/RabbitMqConfig.java
@@ -1,15 +1,21 @@
 package com.a504.qookie.global.rabbitMQ;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.core.env.Environment;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class RabbitMqConfig {
@@ -48,18 +54,6 @@ public class RabbitMqConfig {
     DirectExchange exchange() {
         return new DirectExchange(gptExchange);
     }
-
-//    @Bean
-//    Binding bindingFromSpring(
-//            Queue queueFromSpring,
-//            DirectExchange exchange
-//    ) {
-//        return BindingBuilder
-//                .bind(queueFromSpring)
-//                .to(exchange)
-//                .with(routingKeyToFlask);
-//    }
-
     @Bean
     Binding bindingFromFlask(
             Queue queueFromFlask,
@@ -69,6 +63,15 @@ public class RabbitMqConfig {
                 .bind(queueFromFlask)
                 .to(exchange)
                 .with(routingKeyToSpring);
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        // TODO: delete this before deploying to master
+        factory.setRecoveryInterval(2147483647L);
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
     }
 
     @Bean
@@ -85,6 +88,7 @@ public class RabbitMqConfig {
     public MessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
     @Bean
     public RabbitTemplate rabbitTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
