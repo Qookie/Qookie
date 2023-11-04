7025263751a2942491efe8264805790a62877c79
조원주 <dnjswn9178@hanmail.net>
2023-11-05 02:59:27 +0900

    feat: implement store tabs

--- frontend/src/pages/Store.tsx
+++ frontend/src/pages/Store.tsx
@@ -5,58 +5,90 @@ import Qookie from '../components/shared/molecules/Qookie';
 import Button from '../components/shared/atoms/Button';
 import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import Title from '../components/shared/atoms/Title';
+import ItemTab from '../components/store/organisms/itemTab';
+import { useState } from 'react';
 
 export default function Store() {
   const [qookie, setQookie] = useRecoilState(QookieInfoState);
+  const [currentTab, setCurrentTab] = useState<number>(0);
+
+  const selectTabHandler = (now: number) => {
+    setCurrentTab(now);
+  };
 
   return (
-    <HomeContainer>
-      <QookieContainer>
+    <PageContainer>
+      <TopContainer>
         <BackgroundImg src={qookie.background} alt="bg" />
-        <Qookie {...qookie} />
+        <QookieContainer>
+          <Qookie {...qookie} />
+        </QookieContainer>
         <ButtonContainer>
           <Button theme={'disabled'} size="icon">
             <ShoppingCartIcon width={20} height={20} />
             구매
           </Button>
         </ButtonContainer>
-      </QookieContainer>
+      </TopContainer>
       <BottomContainer>
         <TitleTab>
-          <Title typography="title">상점</Title>
-          <Title typography="title" color="var(--MR_GRAY1)">
+          <Title
+            typography="title"
+            color={currentTab !== 0 ? 'var(--MR_GRAY1)' : 'var(--MR_BLACK)'}
+            onClick={() => selectTabHandler(0)}
+          >
+            상점
+          </Title>
+          <Title
+            typography="title"
+            color={currentTab !== 1 ? 'var(--MR_GRAY1)' : 'var(--MR_BLACK)'}
+            onClick={() => selectTabHandler(1)}
+          >
             MY
           </Title>
         </TitleTab>
+        <ItemTab />
       </BottomContainer>
-    </HomeContainer>
+    </PageContainer>
   );
 }
 
-const HomeContainer = styled.div``;
+const PageContainer = styled.div`
+  width: 100%;
+`;
 
-const QookieContainer = styled.div`
+const TopContainer = styled.div`
   position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  top: 0;
+  width: min(100%, 430px);
+  height: 23rem;
   overflow: hidden;
 `;
 
-const BackgroundImg = styled.img`
+const QookieContainer = styled.div`
   position: absolute;
-  top: 0;
+  top: 40%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  display: inline-flex;
+  justify-content: center;
+  transform: translate(-50%, -50%);
+`;
+
+const BackgroundImg = styled.img`
+  width: 100%;
 `;
 
 const ButtonContainer = styled.div`
-  position: relative;
-  margin-left: auto;
+  position: absolute;
+  right: 0;
+  bottom: 0;
   padding: 0 1rem 1rem 0;
   margin-top: -2rem;
 `;
 
 const BottomContainer = styled.div`
-  padding: 1.5rem 1rem 0rem 1rem;
   width: 100%;
   height: 100%;
   box-sizing: border-box;
@@ -64,5 +96,6 @@ const BottomContainer = styled.div`
 
 const TitleTab = styled.div`
   display: flex;
+  padding: 1.5rem 1rem 0rem 1rem;
   gap: 1.3rem;
 `;
