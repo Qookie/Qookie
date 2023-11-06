dd7da8d9753956ad46441970ff38f9273285f14a
이국신 <nw4611@naver.com>
2023-11-06 10:01:52 +0900

    feat: 퀘스트 타입 정의

---
+++ frontend/src/types/quest.ts
@@ -0,0 +1,22 @@
+export enum QuestId {
+  WAKE = 0,
+  EAT,
+  WALK,
+  SQUAT,
+  PROMISE,
+  PHOTO,
+  STRETCH,
+  MEDITATION,
+  WATER,
+}
+
+export enum Quest {
+  WAKE = 'wake',
+  EAT = 'eat',
+  SQUAT = 'squat',
+  PROMISE = 'promise',
+  PHOTO = 'photo',
+  STRETCH = 'stretch',
+  MEDITATION = 'meditation',
+  WATER = 'WATER',
+}
