32d561e8b452b3f84b15a3ebc68c3f3c3f1a1578
이현복 <nexusin@naver.com>
2023-11-07 16:46:12 +0900

    chore: BottomSheet 애니메이션 -> 트랜지션 변경

--- frontend/src/components/shared/molecules/BottomSheet/index.tsx
+++ frontend/src/components/shared/molecules/BottomSheet/index.tsx
@@ -1,35 +1,33 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import Text from '../../atoms/Text';
 import Button from '../../atoms/Button';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
 interface BottomModalProps {
-  isOpen: boolean | null;
+  isOpen: boolean;
   onClose: () => void;
   title: string;
   children: React.ReactNode;
 }
 
 export default function BottomSheet({ isOpen, onClose, title, children }: BottomModalProps) {
-  if (isOpen !== null) {
-    return (
-      <>
-        <Backdrop onClick={onClose} isOpen={isOpen}>
-          <Container isOpen={isOpen}>
-            <TopConatiner>
-              <Title>{title}</Title>
-              <XMarkIcon width={'1.4rem'} height={'1.4rem'} onClick={onClose} />
-            </TopConatiner>
-            <ChildrenContainer>{children}</ChildrenContainer>
-            <Button theme="default">완료</Button>
-          </Container>
-        </Backdrop>
-      </>
-    );
-  }
+  return (
+    <>
+      <Backdrop onClick={onClose} isOpen={isOpen}>
+        <Container isOpen={isOpen}>
+          <TopConatiner>
+            <Title>{title}</Title>
+            <XMarkIcon width={'1.4rem'} height={'1.4rem'} onClick={onClose} />
+          </TopConatiner>
+          <ChildrenContainer>{children}</ChildrenContainer>
+          <Button theme="default">완료</Button>
+        </Container>
+      </Backdrop>
+    </>
+  );
 }
 
-const Backdrop = styled.div<{ isOpen: boolean | null }>`
+const Backdrop = styled.div<{ isOpen: boolean }>`
   position: fixed;
   top: 0;
   bottom: 0;
@@ -37,20 +35,24 @@ const Backdrop = styled.div<{ isOpen: boolean | null }>`
   z-index: 10;
   width: min(100%, 430px);
   height: 100%;
-  display: flex;
   align-items: center;
   justify-content: center;
-  animation: ${({ isOpen }) => (isOpen ? FadeIn : FadeOut)} 0.3s ease-in-out forwards;
+  visibility: ${({ isOpen }) => (isOpen ? 'visible' : 'hidden')};
+  opacity: ${({ isOpen }) => (isOpen ? '1' : '0')};
+  transition:
+    visibility ease-in-out 0.4s,
+    opacity ease-in-out 0.4s;
 `;
 
-const Container = styled.div<{ isOpen: boolean | null }>`
+const Container = styled.div<{ isOpen: boolean }>`
   border-radius: 12px 12px 0px 0px;
   background: var(--MR_WHITE);
   position: fixed;
   bottom: 0;
   z-index: 20;
   width: min(100%, 430px);
-  animation: ${({ isOpen }) => (isOpen ? SlideUp : SlideDown)} 0.3s ease-in-out forwards;
+  transform: translateY(${({ isOpen }) => (isOpen ? '0%' : '180%')});
+  transition: transform ease-in-out 0.4s;
   padding: 15px 0px;
 `;
 
@@ -66,54 +68,9 @@ const ChildrenContainer = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 180px;
 `;
 
 const Title = styled(Text)`
   font-size: 24px;
   font-weight: 600;
-`;
-
-const SlideUp = keyframes`
-  from {
-    display: none;
-    transform: translateY(180%);
-  }
-  to {
-    display: block;
-    transform: translateY(0%);
-  }
-`;
-
-const SlideDown = keyframes`
-  from {
-    display: block;
-    transform: translateY(0%);
-  }
-  to {
-    display: none;
-    transform: translateY(180%);
-  }
-`;
-
-const FadeIn = keyframes`
-  from {
-    display: none;
-    opacity: 0;
-  }
-  to {
-    display: flex;
-    opacity: 1;
-  }
-`;
-
-const FadeOut = keyframes`
-  from {
-    display: flex;
-    opacity: 1;
-  }
-  to {
-    display: none;
-    opacity: 0;
-  }
 `;
