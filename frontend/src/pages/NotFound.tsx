1418b618e3dafb00c443b14f88bd9c2ba1445302
조원주 <dnjswn9178@hanmail.net>
2023-11-13 12:51:44 +0900

    design: change not found design

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,6 +3,10 @@ import { auth } from '../firebase/firebaseConfig';
 import { signOut } from '@firebase/auth';
 import { useSetRecoilState } from 'recoil';
 import { UserState } from '../modules/user';
+import Error from '../components/shared/atoms/error';
+import Button from '../components/shared/atoms/Button';
+import styled from 'styled-components';
+import TitleLayout from '../components/shared/Template/TitleLayout';
 
 export default function NotFound() {
   const navigate = useNavigate();
@@ -16,12 +20,31 @@ export default function NotFound() {
     });
   };
   return (
-    <>
-      <div>404 NOT FOUND / CHECK YOUR LOGIN STATUS</div>
-      <Link to="/home">to home</Link>
-      <div onClick={logout}>
-        CLICK HERE TO LOGOUT
-      </div>
-    </>
+    <TitleLayout
+      title="Not Found"
+      desc={'로그인 상태를 확인해주세요'}
+      children={
+        <Container>
+          <Error children="404 NOT FOUND" />
+          <Button theme="transparent" onClick={() => navigate('/home')}>
+            홈으로
+          </Button>
+          <Button theme="default" onClick={logout}>
+            로그아웃
+          </Button>
+        </Container>
+      }
+    />
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  padding: 1rem;
+  margin-top: 4rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
