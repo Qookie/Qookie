1bd0028504cf36e7372e121a3da6f2495a306742
이현복 <nexusin@naver.com>
2023-11-13 17:35:40 +0900

    chore: 지난 마음함 BottomSheet, MonthSelector 수정

--- frontend/src/pages/PastMind.tsx
+++ frontend/src/pages/PastMind.tsx
@@ -1,37 +1,69 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import moment, { Moment } from 'moment';
 import Text from '../components/shared/atoms/Text';
-import TitleLayout from '../components/shared/Template/TitleLayout';
 import MonthSelector from '../components/shared/molecules/MonthSelector';
 import MessageCard, { MessageProps } from '../components/mind/molcules/MessageCard';
 import { http } from '../api/instance';
+import BottomDatePicker from '../components/shared/organisms/BottomDatePicker';
 
 export default function PastMind() {
-  const [year, setYear] = useState<string>('2023');
-  const [month, setMonth] = useState<string>('11');
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState<boolean>(false);
+  const [today, setToday] = useState<Moment>(moment());
   const [mindData, setMindData] = useState<MessageProps[]>([]);
 
+  const getMind = async () => {
+    const curYear = today.get('y');
+    const curMonth = String(today.month() + 1).padStart(2, '0');
+    try {
+      const res = await http.get<any>(`/api/heart/list/${curYear}/${curMonth}`);
+      setMindData(res.payload);
+      console.log(mindData);
+    } catch (e) {
+      console.log('coinlist Error : ', e);
+    }
+  };
+
+  const onClose = () => {
+    setIsBottomSheetOpen(false);
+  };
+
+  const onMonthSelectorClick = () => {
+    setIsBottomSheetOpen(true);
+  };
+
+  const onChangeMonth = (nextMonth: number) => {
+    setToday((prev) => prev.clone().month(nextMonth - 1));
+  };
+
+  const onChangeYearMonth = (nextYearMonth: Moment) => {
+    setToday(nextYearMonth.clone());
+    setIsBottomSheetOpen(false);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await http.get<any>(`/api/heart/list/${year}/${month}`);
-        setMindData(res.payload);
-        console.log('mind ', mindData[0])
-      } catch (e) {
-        console.log('getMind', e);
-      }
-    };
-
-    fetchData();
-  }, [year, month]);
-
+    getMind();
+  }, [today]);
+          
   return (
     <>
       <StyledTitle typography='title'>지난 마음</StyledTitle>
-      <MonthSelector />
+        <MonthSelector
+          onClick={onMonthSelectorClick}
+          onClickNextMonth={onChangeMonth}
+          onClickPrevMonth={onChangeMonth}
+          selectedMonth={today.month() + 1}
+        />
       {mindData.map((data, index) => (
         <MessageCard key={index} category={data.category} createdAt={data.createdAt} content={data.content} reply={data.reply} />
       ))}
+      <BottomDatePicker
+        isOpen={isBottomSheetOpen}
+        onClose={onClose}
+        initialTime={today}
+        title="조회 기간"
+        onChangeYearMonth={onChangeYearMonth}
+      />
     </>
   );
 }
