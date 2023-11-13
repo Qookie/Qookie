633aca0be81894160aba860041b26c88f9217526
이국신 <nw4611@naver.com>
2023-11-13 14:50:49 +0900

    feat: 뱃지 아이템 구현

---
+++ frontend/src/components/badge/atoms/BadgeItem/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  condition: number;
+  img: string;
+}
+
+function BadgeItem({ condition, img }: Props) {
+  return (
+    <Container>
+      <img src={img} alt="badge" />
+      <ConditionText>{`누적 ${condition}일`}</ConditionText>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const ConditionText = styled.div`
+  color: var(--MR_GRAY2);
+  font-weight: 600;
+`;
+
+export default BadgeItem;
