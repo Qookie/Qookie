82dea7554c2913b17cfceb6e40670d9db4817e1a
이현복 <nexusin@naver.com>
2023-11-13 17:36:12 +0900

    chore: CSS 수정 및 API 수정

--- frontend/src/components/mind/molcules/MessageCard/index.tsx
+++ frontend/src/components/mind/molcules/MessageCard/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Text from '../../../shared/atoms/Text';
 import OhQookie from '../../../../assets/pngs/ohqookie.png';
 
 export interface MessageProps {
-  category: 'HAPPY' | 'WORRY' | 'THANK' | 'ANXIETY';
+  category: 'HAPPY' | 'WORRY' | 'THANKS' | 'ANXIETY';
   createdAt: string;
   content: string;
   reply: string;
@@ -14,8 +13,10 @@ export default function MessageCard({ category, createdAt, content, reply }: Mes
   return (
     <Container>
       <TopContainer>
-        <Text typography='title'>{category} 일기</Text>
-        <Text typography='main' color='var(--MR_GRAY2)'>{createdAt.toString().split('T')[0]}</Text>
+        <Text typography="title">{category} 일기</Text>
+        <Text typography="main" color="var(--MR_GRAY2)">
+          {createdAt.toString().split('T')[0]}
+        </Text>
       </TopContainer>
 
       <ContentContainer>
@@ -25,17 +26,19 @@ export default function MessageCard({ category, createdAt, content, reply }: Mes
       <ReplyContainer>
         <QookieImg src={OhQookie} width={100} />
         <ReplyDiv>
-          {reply == null ? 
-          <Text color='var(--MR_GRAY2)'>오쿠키 박사가 마음 답장을 작성중이에요!</Text> : (
-            <Text color='var(--MR_BLACK)'>{reply}</Text>)}
+          {reply == null ? (
+            <Text color="var(--MR_GRAY2)">오쿠키 박사가 마음 답장을 작성중이에요!</Text>
+          ) : (
+            <Text color="var(--MR_BLACK)">{reply}</Text>
+          )}
         </ReplyDiv>
       </ReplyContainer>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
-  height: 300px;
+  min-height: 150px;
   border: 1px solid var(--MR_GRAY1);
   border-radius: 12px;
   padding: 20px;
@@ -55,23 +58,23 @@ const ContentContainer = styled.div`
 
 const ReplyContainer = styled.div`
   position: relative;
-  margin: 60px 0;
+  margin-top: 40px;
 `;
 
 const ReplyDiv = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 115px;
+  top: 0;
+  min-height: 115px;
   background: #f8f3eb;
   border: 0.4px solid #e5cc9a;
   border-radius: 8px;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 20px;
 `;
 
 const QookieImg = styled.img`
-  bottom: -21px;
+  top: -40px;
   left: 5%;
   position: absolute;
   z-index: 1;
