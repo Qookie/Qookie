f2aa62af1c0f0e3773472763fc1717c572e86a67
조원주 <dnjswn9178@hanmail.net>
2023-11-14 14:34:52 +0900

    feat: no my qookie

--- frontend/src/pages/Myqookie.tsx
+++ frontend/src/pages/Myqookie.tsx
@@ -4,6 +4,7 @@ import MyqookieListItem, { MyqookieProps } from '../components/mypage/molecules/
 import { useEffect, useState } from 'react';
 import { qookieApi } from '../api';
 import Divider from '../components/shared/atoms/Divider';
+import Error from '../components/shared/atoms/error';
 
 export default function Myqookie() {
   const [myQookieList, setMyQookieList] = useState<MyqookieProps[]>([]);
@@ -17,9 +18,15 @@ export default function Myqookie() {
       title="내가 만든 쿠키"
       children={
         <ListContainer>
-          {myQookieList.map((item, index) => (
-            <MyqookieListItem {...item} age={index + 1} key={index} />
-          ))}
+          {myQookieList.length == 0 ? (
+            myQookieList.map((item, index) => (
+              <MyqookieListItem {...item} age={index + 1} key={index} />
+            ))
+          ) : (
+            <ErrorContainer>
+              <Error children="아직 쿠키가 없어요" />
+            </ErrorContainer>
+          )}
         </ListContainer>
       }
     />
@@ -32,4 +39,8 @@ const ListContainer = styled.div`
   align-items: center;
   padding: 0 1rem;
   box-sizing: border-box;
+`;
+
+const ErrorContainer = styled.div`
+  padding-top: 4rem;
 `;
