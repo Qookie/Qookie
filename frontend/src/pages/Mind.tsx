e05ae80435046751bbf40eb7c903a1cb69eea744
이현복 <nexusin@naver.com>
2023-11-14 10:18:26 +0900

    feat: 마음함 Chip 구현 및 API Response Toast 적용

--- frontend/src/pages/Mind.tsx
+++ frontend/src/pages/Mind.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { http } from '../api/instance';
 import Text from '../components/shared/atoms/Text';
-import TextArea from '../components/mind/atoms/TextArea'
+import TextArea from '../components/mind/atoms/TextArea';
 import Chip from '../components/shared/molecules/Chip';
 import Button from '../components/shared/atoms/Button';
-
 import Letter from '../assets/pngs/letter.png';
 import HandHeart from '../assets/pngs/handheart.png';
 import HandPen from '../assets/pngs/handpen.png';
 import LetterQookie from '../assets/pngs/letterqookie.png';
 import Check from '../assets/pngs/check.png';
-
 import TitleLayout from '../components/shared/Template/TitleLayout';
 import TextImgLayout from '../components/shared/molecules/TextImg';
 import BottomPageLayout from '../components/shared/Template/BottomPageLayout';
-import { useNavigate } from 'react-router-dom';
+import { showToast } from '../components/shared/molecules/Alert';
 
 export default function Mind() {
-  const [inputValue, setInputValue] = useState(''); 
+  const [inputValue, setInputValue] = useState('');
   const [isBottomOpen, setIsBottomOpen] = useState<boolean>(false);
+  const [emotion, setEmotion] = useState<string>('감사');
 
   const navigate = useNavigate();
 
@@ -35,10 +35,13 @@ export default function Mind() {
     try {
       const res = await http.post<any>(url, {
         content: inputValue,
-        category: 'HAPPY',
+        category: emotion,
       });
-      console.log(res.msg)
-      setIsBottomOpen(true)
+      setIsBottomOpen(true);
+      showToast({
+        title: '마음 보내기 완료!',
+        content: '오쿠키 박사님의 답장을 기다려보세요!',
+      });
       return res;
     } catch (e) {
       console.log('postMind', e);
@@ -46,39 +49,98 @@ export default function Mind() {
   };
 
   useEffect(() => {
-    setInputValue('')
-  }, [isBottomOpen])
+    setInputValue('');
+  }, [isBottomOpen]);
+
+  const clickEmotion = (nowEmotion: string) => {
+    setEmotion(nowEmotion);
+  };
 
   return (
     <Container>
       <TitleLayout
-        title='마음 보내기'
+        title="마음 보내기"
         desc={'오늘의 마음을 적어서 보내고,\n오쿠키 박사님의 답장을 기다려보세요.'}
-      >
-        <Chip type='category' icon={<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Smiling%20Face%20with%20Open%20Hands.png" alt="Smiling Face with Open Hands" width="25" height="25" />} text='감사' />
-        <Chip type='category' icon={<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Smiling%20Face%20with%20Hearts.png" alt="Smiling Face with Hearts" width="25" height="25" />} text='행복' />
-        <Chip type='category' icon={<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Confounded%20Face.png" alt="Confounded Face" width="25" height="25" />} text='불안' />
-        <Chip type='category' icon={<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Anxious%20Face%20with%20Sweat.png" alt="Anxious Face with Sweat" width="25" height="25" />} text='걱정' />
-      </TitleLayout>
+      />
+      <ChipContainer>
+        <Chip
+          type="category"
+          setInput={() => clickEmotion('감사')}
+          isClicked={emotion === '감사'}
+          icon={
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Smiling%20Face%20with%20Open%20Hands.png"
+              width="25"
+              height="25"
+            />
+          }
+          text="감사"
+        />
+        <Chip
+          type="category"
+          setInput={() => clickEmotion('행복')}
+          isClicked={emotion === '행복'}
+          icon={
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Smiling%20Face%20with%20Hearts.png"
+              width="25"
+              height="25"
+            />
+          }
+          text="행복"
+        />
+        <Chip
+          type="category"
+          setInput={() => clickEmotion('불안')}
+          isClicked={emotion === '불안'}
+          icon={
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Confounded%20Face.png"
+              width="25"
+              height="25"
+            />
+          }
+          text="불안"
+        />
+        <Chip
+          type="category"
+          setInput={() => clickEmotion('걱정')}
+          isClicked={emotion === '걱정'}
+          icon={
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Anxious%20Face%20with%20Sweat.png"
+              width="25"
+              height="25"
+            />
+          }
+          text="걱정"
+        />
+      </ChipContainer>
 
       <MiddleContainer>
-        <TextArea placeholder='오늘은 어떤 감사함을 느꼈나요?' value={inputValue} onChange={handleChange} />
-        <Counter typography='main' color='var(--MR_GRAY2)'>({inputValue.length} / 2000)</Counter>
-        {inputValue ?
-          (<Button onClick={() => postMind('/api/heart/create')}>보내기</Button>)
-          :
-          (<Button onClick={() => null} theme='disabled'>보내기</Button>)
-        }
+        <TextArea
+          placeholder="오늘은 어떤 감사함을 느꼈나요?"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <Counter typography="main" color="var(--MR_GRAY2)">
+          ({inputValue.length} / 2000)
+        </Counter>
+        {inputValue ? (
+          <Button onClick={() => postMind('/api/heart/create')}>보내기</Button>
+        ) : (
+          <Button onClick={() => null} theme="disabled">
+            보내기
+          </Button>
+        )}
         <PastMind onClick={() => navigate('/past-mind')}>
-          <PastText>
-            지난 마음
-          </PastText>
+          <PastText>지난 마음</PastText>
           <ImgDiv>
             <img src={Letter} />
           </ImgDiv>
         </PastMind>
       </MiddleContainer>
-      
+
       <Divider />
 
       <MiddleContainer>
@@ -91,23 +153,44 @@ export default function Mind() {
       </MiddleContainer>
 
       <BottomContainer>
-        <TextImgLayout image={HandHeart} desc={'오늘 있었던 기분 좋은 일이나 감사한 일, 마음 속에 숨겨두었던 걱정이나 불안 등 스스로의 마음을 표현해 보세요.'} />
-        <TextImgLayout image={HandPen} desc={'마음을 적어내려가는 것 만으로도 생각을 정리하고, 더 깊은 고민으로 빠지는 것을 막을 수 있습니다.'} />
-        <TextImgLayout image={LetterQookie} desc={'조금 기다리면 당신을 위한 상담사 오쿠키 박사님이 답장을 보내줄거예요!'} />
-        <Button theme='transparent' onClick={() => window.scrollTo(0, 0)}>
+        <TextImgLayout
+          image={HandHeart}
+          desc={
+            '오늘 있었던 기분 좋은 일이나 감사한 일, 마음 속에 숨겨두었던 걱정이나 불안 등 스스로의 마음을 표현해 보세요.'
+          }
+        />
+        <TextImgLayout
+          image={HandPen}
+          desc={
+            '마음을 적어내려가는 것 만으로도 생각을 정리하고, 더 깊은 고민으로 빠지는 것을 막을 수 있습니다.'
+          }
+        />
+        <TextImgLayout
+          image={LetterQookie}
+          desc={
+            '조금 기다리면 당신을 위한 상담사 오쿠키 박사님이 답장을 보내줄거예요!'
+          }
+        />
+        <Button theme="transparent" onClick={() => window.scrollTo(0, 0)}>
           <img src={Letter} width={25} height={25} />
           &nbsp;마음 보내기
         </Button>
       </BottomContainer>
-      <BottomPageLayout isopen={isBottomOpen} onCloseRequest={openBottomHandler} children={
-        <BottomPageContainer>
-          <img src={Check} />
-          <BottomPageText typography='main' color='var(--MR_GRAY2)'>{'마음 보내기가\n완료되었습니다.'}</BottomPageText>
-          <Button onClick={() => navigate('/')}>마음 확인하기</Button>
-        </BottomPageContainer>
-      } />
+      <BottomPageLayout
+        isopen={isBottomOpen}
+        onCloseRequest={openBottomHandler}
+        children={
+          <BottomPageContainer>
+            <img src={Check} />
+            <BottomPageText typography="main" color="var(--MR_GRAY2)">
+              {'마음 보내기가\n완료되었습니다.'}
+            </BottomPageText>
+            <Button onClick={() => navigate('/')}>마음 확인하기</Button>
+          </BottomPageContainer>
+        }
+      />
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
@@ -180,4 +263,10 @@ const BottomPageText = styled(Text)`
 const Counter = styled(Text)`
   text-align: end;
   margin-bottom: 10px;
+`;
+
+const ChipContainer = styled.div`
+  padding: 0 1rem;
+  display: flex;
+  justify-content: space-around;
 `;
