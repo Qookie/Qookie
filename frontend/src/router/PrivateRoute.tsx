b3b68e32c72ddaf0d17f6fc9c21e6d70bc9cd499
김동훈 <hunn000@gmail.com>
2023-11-15 11:10:05 +0900

    refactor: remove console log

--- frontend/src/router/PrivateRoute.tsx
+++ frontend/src/router/PrivateRoute.tsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 export const PrivateRoute = ({ signedIn }: { signedIn: boolean }): React.ReactElement => {
   const location = useLocation();
-  console.log(location, signedIn);
 
   if (location.pathname === '/' && signedIn) {
     return <Navigate to="/home" />;
