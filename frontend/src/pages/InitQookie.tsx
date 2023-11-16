711b3eda23bc2091942bab3263f6e1b90a4ead4e
조원주 <dnjswn9178@hanmail.net>
2023-11-16 11:21:21 +0900

    feat: add loading

--- frontend/src/pages/InitQookie.tsx
+++ frontend/src/pages/InitQookie.tsx
@@ -11,6 +11,7 @@ import { useNavigate } from 'react-router-dom';
 import Qookie from '../components/shared/molecules/Qookie';
 import { useRecoilValue } from 'recoil';
 import { QookieInfoState } from '../modules/qookie';
+import Spinner from '../components/shared/atoms/Spinner';
 
 enum Step {
   Custom,
@@ -27,6 +28,7 @@ interface Select {
 
 function InitQookie() {
   const [step, setStep] = useState<Step>(Step.Custom);
+  const [isDataLoading, setIsDataLoading] = useState<boolean>(false);
   const qookie = useRecoilValue(QookieInfoState);
 
   const navigate = useNavigate();
@@ -76,6 +78,7 @@ function InitQookie() {
   };
 
   const getQookieDesign = async () => {
+    setIsDataLoading(true);
     const {
       payload: { eyes, mouths },
     } = await http.get<QookieFaceOptionResponse>('/api/cookie/face/list');
@@ -92,6 +95,8 @@ function InitQookie() {
       mouthImg: mouths[0]?.image ?? '',
       name: '',
     }));
+
+    setIsDataLoading(false);
   };
 
   const onClickNext = () => {
@@ -114,37 +119,43 @@ function InitQookie() {
   }, []);
 
   return (
-    <TitleLayout
-      title="반죽을 꾸며주세요!"
-      desc={'첫 번째 성장을 함께 할 반죽이 도착했어요!\n어울리는 눈과 입을 만들어주세요.'}
-    >
-      <MainContainer>
-        <Qookie {...qookie} eye={eyeImg} mouth={mouthImg} level={1} body={Dough} />
-        {step === Step.Custom ? (
-          <>
-            <FaceOptionSelctor
-              label={'눈'}
-              optionData={eyes}
-              selected={eye}
-              onSelectItem={onSelectEye}
-            />
-            <FaceOptionSelctor
-              label={'입'}
-              optionData={mouths}
-              selected={mouth}
-              onSelectItem={onSelectMouth}
-            />
-          </>
-        ) : (
-          <Input placeholder="이름을 지어주세요" onChange={onChangeQookieName} />
-        )}
-      </MainContainer>
-      <ButtonCotainer>
-        <Button onClick={onClickNext} style={{ marginTop: 'auto' }}>
-          다음
-        </Button>
-      </ButtonCotainer>
-    </TitleLayout>
+    <>
+      {isDataLoading ? (
+        <Spinner />
+      ) : (
+        <TitleLayout
+          title="반죽을 꾸며주세요!"
+          desc={'첫 번째 성장을 함께 할 반죽이 도착했어요!\n어울리는 눈과 입을 만들어주세요.'}
+        >
+          <MainContainer>
+            <Qookie {...qookie} eye={eyeImg} mouth={mouthImg} level={1} body={Dough} />
+            {step === Step.Custom ? (
+              <>
+                <FaceOptionSelctor
+                  label={'눈'}
+                  optionData={eyes}
+                  selected={eye}
+                  onSelectItem={onSelectEye}
+                />
+                <FaceOptionSelctor
+                  label={'입'}
+                  optionData={mouths}
+                  selected={mouth}
+                  onSelectItem={onSelectMouth}
+                />
+              </>
+            ) : (
+              <Input placeholder="이름을 지어주세요" onChange={onChangeQookieName} />
+            )}
+          </MainContainer>
+          <ButtonCotainer>
+            <Button onClick={onClickNext} style={{ marginTop: 'auto' }}>
+              다음
+            </Button>
+          </ButtonCotainer>
+        </TitleLayout>
+      )}
+    </>
   );
 }
 
