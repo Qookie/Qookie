256082fee77f00b656910cfcae7cca3cd7b50145
이현복 <nexusin@naver.com>
2023-11-16 14:02:19 +0900

    chore: stories props 수정

--- frontend/src/components/challenge/molecules/ChallengeCard/ChallengeCard.stories.tsx
+++ frontend/src/components/challenge/molecules/ChallengeCard/ChallengeCard.stories.tsx
@@ -11,9 +11,13 @@ type Story = StoryObj<typeof ChallengeCard>;
 
 export const Default: Story = {
   args: {
-    title: '15일 기상 챌린지',
-    condition: '15일 / 15일',
+    challengeName: '15일 기상 챌린지',
     coin: 100,
+    curCnt: 15,
+    totalCnt: 15,
+    questName: 'WAKE',
+    status: 'incomplete',
+    badgeId: 0,
   },
   render: (args) => <ChallengeCard {...args} />,
 };
--- frontend/src/components/shared/molecules/TimePicker/TimePicker.stories.tsx
+++ frontend/src/components/shared/molecules/TimePicker/TimePicker.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import TimePicker from '.';
+import moment from 'moment';
 
 const meta: Meta<typeof TimePicker> = {
   component: TimePicker,
@@ -12,12 +13,6 @@ type Story = StoryObj<typeof TimePicker>;
 
 export const Default: Story = {
   args: {
-    time: {
-      hour: '12',
-      minute: '34',
-      meridiem: 'PM',
-    },
   },
-
   render: (args) => <TimePicker {...args} />,
 };
